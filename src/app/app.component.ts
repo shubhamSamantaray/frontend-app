744b866d17ae1aee7293a12b6a21a44583bdb1e1
shubhamSamantaray <samantaray.shubham423@gmail.com>
2026-01-02 16:57:53 +0530

    Day 1: Angular setup and basic component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend-app';
+  title = 'Full Stack Journey – Day 1';
 }
